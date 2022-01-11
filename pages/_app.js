c7d2947abd76ad79b4daae777a8acb81db4e6957
Thomas <thomas.new.it@gmail.com>
2022-01-11 14:53:43 +0500

    передача и отрисовка данных с jsonplaceholder

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Heading from '../components/Heading';
 import styles from '../styles/Home.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.wrapper}>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Heading text="This is React & Next :)" />
     </div>
   );
