d008a8670fb5145c4f09a2b659591d0c4e48ac35
Thomas <thomas.new.it@gmail.com>
2022-01-11 14:11:13 +0500

    рефакторинг + кейс с ошибкой с перебросом на главную

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import Heading from './Heading';
 
 const Footer = () => {
   return (
-    <>
+    <footer>
       <Heading tag="h3" text="Footer" />
-    </>
+    </footer>
   );
 };
 
--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,27 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Heading from '../components/Heading';
+import styles from '../styles/404.module.scss';
 
 const Error = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 2000);
+  }, [router]); // через 2 секунды перебросит на главную
+
   return (
-    <>
-      <Heading text="404" />
-      <Heading tag="h2" text="Что-то пошло не так..." />
-    </>
+    <div className={styles.wrapper}>
+      <div>
+        <Heading text="404" />
+        <Heading tag="h2" text="Что-то пошло не так..." />
+      </div>
+    </div>
   );
 };
 
 export default Error;
+
+// ошибка 404 идет сразу из коробки
