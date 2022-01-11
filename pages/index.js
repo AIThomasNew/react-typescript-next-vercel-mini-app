29c9c70856ad44bd9932725536585fc58fa070d9
Thomas <thomas.new.it@gmail.com>
2022-01-11 18:13:45 +0500

    1 исправление

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,20 @@ import Heading from '../components/Heading';
 import styles from '../styles/Home.module.scss';
 
 const Home = () => {
+  const inputText = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div className={styles.wrapper}>
       <Head>
         <title>Home</title>
       </Head>
       <Heading text="This is React & Next :)" />
+      <div>
+        <p>onChange input</p>
+        <input onChange={inputText} type="text" />
+      </div>
     </div>
   );
 };
